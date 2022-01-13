85ba9b459c5e09d58a50569ef8e8ba18ed38cc40
Gabriela <gabibizy@hotmail.com>
2022-01-13 15:46:59 -0300

    crud utilizando flask e sqlalchemy

---
+++ main.py
@@ -0,0 +1,82 @@
+from app import app
+from flask import flash, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/crud'
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+	id = db.Column(db.Integer, primary_key= True)
+	nome = db.Column(db.String(200))
+	email = db.Column(db.String(150))
+
+
+@app.route('/')
+def users():
+	try:
+		lista_usuarios = Usuario.query.all()
+		return render_template('users.html', lista_usuarios=lista_usuarios)
+	except Exception as e:
+		print(e)
+
+
+@app.route('/new_user')
+def add_user_view():
+	return render_template('add.html')
+		
+
+@app.route('/add', methods=['POST'])
+def add_user():
+	try:
+		usuario = Usuario(nome = request.form.get("nome"), email = request.form.get('email'))
+
+		db.session.add(usuario)
+		db.session.commit()
+
+		flash('User added successfully!')
+		return redirect('/')
+		
+	except Exception as e:
+		print('Erro', e)
+
+
+@app.route('/select/<int:id>')
+def select_by_id(id):
+	try:
+		usuario = Usuario.query.filter_by(id=id).first()
+		return render_template('edit.html', usuario=usuario)
+	except Exception as e:
+		print(e)
+
+
+@app.route('/update/<int:id>', methods=['POST'])
+def update_user(id):
+	usuario = Usuario.query.filter_by(id=id).first() 
+
+	usuario.nome = request.form.get("nome")
+	usuario.email = request.form.get("email")
+
+	db.session.add(usuario)
+	db.session.commit()
+
+	flash('User updated successfully!')
+	return redirect('/')
+
+		
+@app.route('/delete/<int:id>')
+def delete_user(id):
+
+	usuario = Usuario.query.filter_by(id=id).first()
+	try:
+		db.session.delete(usuario)
+		db.session.commit()
+		flash('User deleted successfully!')
+		return redirect('/')
+
+	except Exception as e:
+		print('Erro', e)
+		
+
+if __name__ == "__main__":
+    app.run(debug=True)
